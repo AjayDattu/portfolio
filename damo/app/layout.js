f1198e7ab0f31b511764555516f8e614fbb050c9
Ajay Dattu <dattuajay005@gmail.com>
2024-11-10 10:49:47 +0530

    #added navbar

--- damo/app/layout.js
+++ damo/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Header from "../components/header"
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Header className="z-20" />
         {children}
       </body>
     </html>
