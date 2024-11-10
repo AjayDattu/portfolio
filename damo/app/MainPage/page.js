9a5732e832145a0ca437ec4d0572622e2dde5cd0
Ajay Dattu <dattuajay005@gmail.com>
2024-11-10 17:15:24 +0530

    #error resolved

--- damo/app/MainPage/page.js
+++ damo/app/MainPage/page.js
@@ -10,7 +10,7 @@ function Page() {
   const words = ["Frontend Developer", "Coder", "Youtuber", "Designer"];
   return (
     <div className="bg-black h-full flex flex-col items-center justify-center ">
-      <BackgroundBeamsWithCollision className="flex flex-col gap-6 text-left items-center px-4 z-50">
+      <BackgroundBeamsWithCollision className="flex flex-col gap-6 text-left items-center px-4">
         <Svg />
         <div className="w-full flex flex-col px-[400px] sm:flex-row items-center sm:items-start text-red-500 gap-3 sm:gap-6 md:gap-9 ">
           <span className="text-4xl sm:text-4xl md:text-5xl lg:text-6xl">a</span>
