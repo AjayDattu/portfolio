b7866ae00f22239f8e8d7f8fdd4e3651d110ef14
Ajay Dattu <dattuajay005@gmail.com>
2024-11-16 19:45:43 +0530

    #small changes on About page

--- damo/app/About/page.js
+++ damo/app/About/page.js
@@ -66,33 +66,35 @@ const ColorSections = () => {
   return (
     <main className="root">
       <section id="id" data-color="#000" className="relative section min-h-screen flex items-center justify-center s-1">
+      <div>
       <Particles
-        className="absolute inset-0"
-        quantity={1000}
-        ease={80}
-        color={"#ffffff"}
-        refresh
-      />
-      <Meteors number={4} />
-      <iframe className="absolute right-0 top-0 w-[50%] h-full" src="https://lottie.host/embed/d9d30006-a4b0-4134-84ec-c9ead95d03cb/vEe1dsNi31.json"></iframe>
-         <div className="absolute left-10 bottom-28 flex flex-col">
-            <div className="text-[#fff] text-1xl font-bold">
-              <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-                  <p className="text-xl font-semibold">
-                    I am from India<span className="text-[#5046e6]">.</span>
-                  </p>
-              </BoxReveal>
-            </div>
-            <div className="text-[#455CE9] text-4xl font-bold">
-               <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-                  <p className="text-5xl font-semibold" id="h">
-                   Everyone Call me Damu<span className="text-[#5046e6]">.</span>
-                  </p>
-              </BoxReveal>
-            </div>
-         </div>
+          className="absolute inset-0"
+          quantity={1000}
+          ease={80}
+          color={"#ffffff"}
+          refresh
+        />
+        <Meteors number={4} />
+        <iframe className="absolute right-0 top-0 w-[50%] h-full" src="https://lottie.host/embed/d9d30006-a4b0-4134-84ec-c9ead95d03cb/vEe1dsNi31.json"></iframe>
+          <div className="absolute left-10 bottom-28 flex flex-col">
+              <div className="text-[#fff] text-1xl font-bold">
+                <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                    <p className="text-xl font-semibold">
+                      I am from India<span className="text-[#5046e6]">.</span>
+                    </p>
+                </BoxReveal>
+              </div>
+              <div className="text-[#455CE9] text-4xl font-bold">
+                <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                    <p className="text-5xl font-semibold" id="h">
+                    Everyone Call me Damu<span className="text-[#5046e6]">.</span>
+                    </p>
+                </BoxReveal>
+              </div>
+          </div>
+         </div> 
       </section>
-      <section data-color="#000" className="relative section min-h-screen flex items-center justify-center s-2">
+      <div className="relative section min-h-screen flex items-center justify-center s-2 bg-black">
       <Particles
         className="absolute inset-0"
         quantity={1000}
@@ -105,8 +107,8 @@ const ColorSections = () => {
                 <iframe className="absolute w-[30%] h-[50%] left-0 top-0" src="https://lottie.host/embed/2b86e896-558c-4255-9f53-16f715871ff8/lqLhvKteyP.json"/> 
                 <div className='md:text-[100px] lg:text-[300px]'>Education</div>
          </div>
-      </section>
-     <section data-color="#000" className="relative section h-screen flex items-center justify-center s-3 italic" id='e'>
+      </div>
+     <div  className="relative section h-screen flex items-center justify-center s-3 italic bg-black" id='e'>
         <Particles
         className="absolute inset-0"
         quantity={500}
@@ -130,8 +132,8 @@ const ColorSections = () => {
           </div>
 
         </div>
-      </section>
-      <section data-color="#000" className="relative section h-screen flex items-center justify-center s-4 gpa-10" id='e'>
+      </div>
+      <div data-color="#000" className="relative section h-screen flex items-center justify-center s-4 gpa-10 bg-black" id='e'>
        <Particles
         className="absolute inset-0"
         quantity={1000}
@@ -155,7 +157,7 @@ const ColorSections = () => {
             Gudlavalleru 2019-2022
           </div>
         </div>
-      </section>
+      </div>
        <section data-color="#05377b" className="relative section h-screen flex items-center justify-center s-4 gpa-10" id='e'>
        <Particles
         className="absolute inset-0"
